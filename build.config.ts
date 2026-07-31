fea6a11fe2a6f7b83b4fef92617a292070ddfaab
Bineet Naidu <bineet.s.naidu@gmail.com>
2026-07-31 12:33:17 +0530

    chore: setup tsconfig, unbuild, oxlint, prettier and vitest configs

---
+++ build.config.ts
@@ -0,0 +1,17 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  entries: [
+    'src/index',
+    {
+      input: 'src/cli/index',
+      name: 'cli',
+    },
+  ],
+  declaration: true,
+  clean: true,
+  rollup: {
+    emitCJS: false,
+    inlineDependencies: true,
+  },
+});
